c20c2949bed5373a47f2fa01b1da884c86fb27cb
Christian Ledermann <christian.ledermann@gmail.com>
2016-05-10 17:06:36 +0100

    Add OSMChangeObject

--- osschooldata/schools/osmoapi.py
+++ osschooldata/schools/osmoapi.py
@@ -8,6 +8,43 @@ from requests_oauthlib import OAuth1Session
 API_URL = 'http://api06.dev.openstreetmap.org/'
 
 
+class OsmChange(object):
+
+    """
+    http://wiki.openstreetmap.org/wiki/OsmChange
+    """
+
+    def __init__(self):
+        self.nodes = []
+        self.ways = []
+        self.multipolygons = []
+        self.idx = 0
+
+    def create_node(self, point, **kwargs):
+        self.idx -= 1
+        self.nodes.append(dict(id=self.idx, lat=point.x, lon=point.y, tags=dict(**kwargs))
+        return self.idx
+
+    def etree_element(self):
+        root = etree.Element('osmChange')
+        create_element = etree.SubElement(root, 'create')
+        for node in self.nodes:
+            node_element = etree.SubElement(create_element, 'node')
+            node_element.set('id', node['id'])
+            node_element.set('lat', node['lat'])
+            node_element.set('lon', node['lon'])
+            for k, v in node['tags'].items():
+                tag_element = etree.SubElement(node_element, 'tag')
+                tag_element.set('k', k)
+                tag_element.set('v', v)
+        return root
+
+    def to_string(self):
+        return etree.tostring(
+                self.etree_element(),
+                encoding='utf-8').decode('UTF-8')
+
+
 class ChangeSet(object):
 
     def __init__(self, id=None, created_by=None, comment=None):
@@ -34,6 +71,7 @@ class ChangeSet(object):
 
 
 class OSMOAuthAPI(object):
+
     """OSM API with OAuth."""
 
     def __init__(self, client_key, client_secret, resource_owner_key, resource_owner_secret):
@@ -57,5 +95,3 @@ class OSMOAuthAPI(object):
             return True
         else:
             return False
-
-
