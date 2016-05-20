fb6703e884c7c9e2fe715ae3700176743d5b48d8
Christian Ledermann <christian.ledermann@gmail.com>
2016-05-20 17:25:37 +0100

    Dockerize me

--- install.sh
+++ install.sh
@@ -8,3 +8,18 @@ source django/bin/activate
 pip install -r django/requirements.txt
 source fabric/bin/activate
 pip install -r fabric/requirements.txt
+
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get update
+sudo apt-get purge lxc-docker
+apt-cache policy docker-engine
+sudo apt-get install linux-image-extra-$(uname -r)
+sudo apt-get install apparmor
+sudo apt-get install docker-engine
+sudo pip install docker-compose
+curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > ~/bin/docker-machine && \
+chmod +x ~/bin/docker-machine
+
+docker-machine create -d virtualbox dev;
+eval "$(docker-machine env dev)"
