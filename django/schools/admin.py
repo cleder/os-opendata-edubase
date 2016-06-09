647a3a114897755791cde730c59c13226a0fb3bb
Christian Ledermann <christian.ledermann@gmail.com>
2016-06-09 09:00:52 +0100

    Improve Admin

--- django/schools/admin.py
+++ django/schools/admin.py
@@ -7,9 +7,25 @@ from .models import EducationSite, Postcodes, School, Multipolygons, ImportLog
 class ImportLogAdmin(admin.ModelAdmin):
     raw_id_fields = ['site', 'school' ]
     list_display = ['user', 'created', 'changeset']
+    search_fields = ['user', ]
 
-admin.site.register(EducationSite, admin.GeoModelAdmin)
-admin.site.register(Postcodes, admin.GeoModelAdmin)
-admin.site.register(School, admin.GeoModelAdmin)
+class EducationSiteAdmin(admin.GeoModelAdmin):
+    list_display=['grid_ref', 'distname']
+    search_fields = ['grid_ref', 'distname']
+
+
+class PostcodesAdmin(admin.GeoModelAdmin):
+    list_display=['postcode',]
+    search_fields = ['postcode',]
+
+
+class SchoolAdmin(admin.GeoModelAdmin):
+    list_display=['source', 'local_authority', 'schoolname', 'postcode']
+    search_fields = ['postcode', 'local_authority', 'schoolname']
+
+
+admin.site.register(EducationSite, EducationSiteAdmin)
+admin.site.register(Postcodes, PostcodesAdmin)
+admin.site.register(School, SchoolAdmin)
 admin.site.register(Multipolygons, admin.GeoModelAdmin)
 admin.site.register(ImportLog, ImportLogAdmin)
