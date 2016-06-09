4a93b63128ceac064d91db8cbf34dfc30a653945
Christian Ledermann <christian.ledermann@gmail.com>
2016-06-09 08:48:56 +0100

    make importlog admin easier to read

--- django/schools/admin.py
+++ django/schools/admin.py
@@ -4,9 +4,12 @@ from __future__ import unicode_literals
 from django.contrib.gis import admin
 from .models import EducationSite, Postcodes, School, Multipolygons, ImportLog
 
+class ImportLogAdmin(admin.ModelAdmin):
+    raw_id_fields = ['site', 'school' ]
+    list_display = ['user', 'created', 'changeset']
 
 admin.site.register(EducationSite, admin.GeoModelAdmin)
 admin.site.register(Postcodes, admin.GeoModelAdmin)
 admin.site.register(School, admin.GeoModelAdmin)
 admin.site.register(Multipolygons, admin.GeoModelAdmin)
-admin.site.register(ImportLog, admin.GeoModelAdmin)
+admin.site.register(ImportLog, ImportLogAdmin)
