d695e92d7c213b2c72b2c2f88231472f969d55bd
Christian Ledermann <christian.ledermann@gmail.com>
2016-10-05 18:56:36 +0100

    remove unused pipelines, tweak UI

--- django/osschooldata/settings.py
+++ django/osschooldata/settings.py
@@ -134,14 +134,10 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.auth_allowed',
     'social.pipeline.social_auth.social_user',
     'social.pipeline.user.get_username',
-#    '.pipeline.require_email',
-    'social.pipeline.mail.mail_validation',
     'social.pipeline.user.create_user',
     'social.pipeline.social_auth.associate_user',
-    'social.pipeline.debug.debug',
     'social.pipeline.social_auth.load_extra_data',
     'social.pipeline.user.user_details',
-    'social.pipeline.debug.debug'
 )
 
 
